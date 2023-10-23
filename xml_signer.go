540ef96dd2a389c5a562846d6033d98e9f96498a
akkahshh.a <akkahshh.a@npci.org.in>
2023-10-23 12:15:27 +0530

    added fields in struct and removed return

--- xml_signer.go
+++ xml_signer.go
@@ -16,11 +16,13 @@ func init() {
 }
 
 type XmlSigner struct {
-	// SignedXml string
-	// TxnId     string
+	PrivateKey crypto.Signer
+	CertBytes  []byte
+	SignedXml  string
+	TxnId      string
 }
 
-func (x *XmlSigner) GetPrivateKeyAndCert(p12FilePath, password string) (crypto.Signer, []byte) {
+func (x *XmlSigner) GetPrivateKeyAndCert(p12FilePath, password string) {
 	p12Bytes, err := os.ReadFile(p12FilePath)
 	if err != nil {
 		log.Fatalf("failed to read .p12 file: %v", err)
@@ -45,7 +47,8 @@ func (x *XmlSigner) GetPrivateKeyAndCert(p12FilePath, password string) (crypto.S
 
 	certPEMBytes := pem.EncodeToMemory(certPEM)
 
-	return signer, certPEMBytes
+	x.PrivateKey = signer
+	x.CertBytes = certPEMBytes
 }
 
 // func (x *XmlSigner) GetSignedXmlAndTxnId(signer crypto.Signer, cert []byte, payloadStr string) (string, string, error) {
