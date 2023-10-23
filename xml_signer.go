5753d1dfb34b29e0ba8263984d15d1257692f2c6
akkahshh.a <akkahshh.a@npci.org.in>
2023-10-23 12:04:11 +0530

    changed error handling

--- xml_signer.go
+++ xml_signer.go
@@ -4,7 +4,7 @@ import (
 	"crypto"
 	"crypto/rsa"
 	"encoding/pem"
-	"fmt"
+	"log"
 	"os"
 
 	"go.k6.io/k6/js/modules"
@@ -20,20 +20,20 @@ type XmlSigner struct {
 	// TxnId     string
 }
 
-func (x *XmlSigner) GetPrivateKeyAndCert(p12FilePath, password string) (crypto.Signer, []byte, error) {
+func (x *XmlSigner) GetPrivateKeyAndCert(p12FilePath, password string) (crypto.Signer, []byte) {
 	p12Bytes, err := os.ReadFile(p12FilePath)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to read .p12 file: %v", err)
+		log.Fatalf("failed to read .p12 file: %v", err)
 	}
 
 	privateKey, cert, err := pkcs12.Decode(p12Bytes, password)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to decode .p12 file: %v", err)
+		log.Fatalf("failed to decode .p12 file: %v", err)
 	}
 
 	rsaPrivateKey, ok := privateKey.(*rsa.PrivateKey)
 	if !ok {
-		return nil, nil, fmt.Errorf("private key is not of type *rsa.PrivateKey")
+		log.Fatalf("private key is not of type *rsa.PrivateKey")
 	}
 
 	signer := crypto.Signer(rsaPrivateKey)
@@ -45,7 +45,7 @@ func (x *XmlSigner) GetPrivateKeyAndCert(p12FilePath, password string) (crypto.S
 
 	certPEMBytes := pem.EncodeToMemory(certPEM)
 
-	return signer, certPEMBytes, nil
+	return signer, certPEMBytes
 }
 
 // func (x *XmlSigner) GetSignedXmlAndTxnId(signer crypto.Signer, cert []byte, payloadStr string) (string, string, error) {
